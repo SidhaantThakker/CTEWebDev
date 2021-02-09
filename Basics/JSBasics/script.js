e33bb676c73bf4dc55fd2555cbe7b092ef41d36d
Sidhaant Thakker <sidhaantthakker@gmail.com>
2021-02-09 21:17:22 +0530

    adds js and bootstrap

---
+++ Basics/JSBasics/script.js
@@ -0,0 +1,13 @@
+function foo(a, b){
+ console.log("Hi from foo!")
+ console.log(a+b)
+}
+
+const abc = (c,d) => {
+    console.log("Hi from abc!")
+    console.log(c+d)
+}
+
+const refunc = () => {
+    document.write(10);
+}
